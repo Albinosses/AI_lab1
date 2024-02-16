2f2c0836995982e69325fb818390a5a8b5d0bcf5
Vasyil Syman <vsyman9@gmail.com>
2024-02-16 18:22:05 +0200

    feat: working questionnaire

---
+++ src/levels/NoviceLevel.js
@@ -0,0 +1,8 @@
+import React, { useState } from 'react';
+
+const NoviceLevel = ({ handleChange }) => (
+    <div>
+        <h3>Новачок (Novice)</h3>
+        {/* Render questions for novice level */}
+    </div>
+);
